39e2495b4bcb5a452f80ac907d67dd943d9c11ef
romanesss <romagavrilyuk2015@gmail.com>
2024-12-24 20:19:22 +0200

    quick fixes

--- src/tests/api-login.spec.ts
+++ src/tests/api-login.spec.ts
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test';
 import { test } from '../fixtures/baseFixture';
 import Credentials from '../data/credentials';
 
-test('Api login', async ({ app, app: { shopPage }, page }) => {
+test('Api login', async ({ app, page, app: { shopPage } }) => {
     await app.authorization(Credentials.getUserCredentials('standard'));
     await shopPage.open();
     expect(page.url()).toContain(shopPage.url);
