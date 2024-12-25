4eb5bd8bfd5597298cdcd6ace8168ef7eea5ed9e
romanesss <romagavrilyuk2015@gmail.com>
2024-12-25 23:36:06 +0200

    Id add

--- src/tests/ui-tests/login.spec.ts
+++ src/tests/ui-tests/login.spec.ts
@@ -2,20 +2,20 @@ import { expect } from '@playwright/test';
 import { test } from '../../fixtures/baseFixture';
 import { TestUser } from '../../models/test-user.model';
 
-test.describe('Login', () => {
+test.describe('Login @S9fe64d91', () => {
     let testUser: TestUser;
     test.beforeEach(async ({ acceptanceHelpers }) => {
         testUser = await acceptanceHelpers.generateRandomUser();
     });
 
-    test('Ui login', async ({ page, app: { loginPage, shopPage } }) => {
+    test('Ui login @T3fad3685', async ({ page, app: { loginPage, shopPage } }) => {
         await loginPage.open();
         await loginPage.login({ email: testUser.email, password: testUser.password });
         expect(page.url()).toContain(shopPage.url);
         await expect(shopPage.header.freeShoppingButton).toBeVisible();
     });
 
-    test('Headless login', async ({ app, page, app: { shopPage } }) => {
+    test('Headless login @Tada19e55', async ({ app, page, app: { shopPage } }) => {
         await app.headlessLogin({ email: testUser.email, password: testUser.password });
         await shopPage.open();
         expect(page.url()).toContain(shopPage.url);
